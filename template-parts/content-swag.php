2e69041d6e259d8194c8d597b3b8c3dbc7ee1d0a
Ryan <ryan.dimascio@gmail.com>
2018-03-11 12:30:07 -0700

    fix: grid-item bg

--- template-parts/content-swag.php
+++ template-parts/content-swag.php
@@ -12,17 +12,20 @@
 <?php
 $args = array( 'post_type' => 'brand', 'posts_per_page' => 16, 'orderby' => 'date', 'order' => 'ASC' );
 $loop = new WP_Query( $args );
+$brand_cat = get_the_category();
 
 while ( $loop->have_posts() ) : $loop->the_post(); ?>
 
-    <article id="post-<?php the_ID(); ?>" <?php post_class("grid-item all"); ?>>
-        <a data-toggle="modal" class="open_post" data-target="#postModal" href="<?php the_permalink(); ?>">
+
+
+    <article id="post-<?= the_ID(); ?>" <?= post_class('grid-item all'); ?>>
+        <a data-toggle="modal" class="open_post" data-target="#postModal" href="<?= the_permalink(); ?>">
             <div class="item-header flex">
                 <div class="item-logo flex justify-start items-center">
-                    <img src="http://cdn.swag.space/icons/<?php the_field('brand_logo'); ?>.svg" class="logo" />
+                    <img src="http://cdn.swag.space/icons/<?= the_field('brand_logo'); ?>.svg" class="logo" />
                 </div>
                 <div class="item-title flex justify-between items-center">
-                    <h5><?php the_title(); ?></h5>
+                    <h5><?= the_title(); ?></h5>
                 </div>
             </div><!-- .item-header -->
             <div class="item-image" style="background-image: url(<?php the_post_thumbnail_url(); ?>)"></div>
