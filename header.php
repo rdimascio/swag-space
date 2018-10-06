bf295b6e3c943c844332c9d25fba6db4e3695dc2
Ryan DiMascio <ryan.dimascio@gmail.com>
2018-10-06 13:11:19 -0700

    add GA tracking script

--- header.php
+++ header.php
@@ -32,6 +32,17 @@
     <!-- Place favicon.ico in the root directory -->
 
 	<?php wp_head(); ?>
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-127032697-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-127032697-1');
+    </script>
+
 </head>
 
 <body <?php body_class(); ?>>
